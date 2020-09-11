516466679d1f034bb47d71159da5f8d141ea7bb5
WhitneyDluhosh <whitneydluhosh@gmail.com>
2020-09-11 13:27:01 -0700

    Snapshot Testing push

--- client/test/snapshot.test.js
+++ client/test/snapshot.test.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Link from '../Link.react';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Link page="https://workbc-grants-dev.pathfinder.gov.bc.ca/">WorkBC Grants</Link>)
-    .toJSON();
+test('renders correctly', () => {
+  const component = renderer.create(
+  <Link page="https://workbc-grants-dev.pathfinder.gov.bc.ca/">WorkBC Grants</Link>,
+  );
+
+  let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+
 });
