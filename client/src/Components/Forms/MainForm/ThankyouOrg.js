0e2bd3d363a857695d3b34ddc7b2bf1459b486c2
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-09-29 16:53:26 -0700

    updated confirmation page
    updated confirmation page for organization - first take

--- client/src/Components/Forms/MainForm/ThankyouOrg.js
+++ client/src/Components/Forms/MainForm/ThankyouOrg.js
@@ -1,36 +1,35 @@
-import React, {Component} from 'react'
-import { BsCheckCircle } from "react-icons/bs"
-import { IconContext } from 'react-icons'
+import React, { Component } from 'react'
+
 
 class ThankyouOrg extends Component {
     render() {
+        const applicationId = (this.props.location.state !== undefined ? this.props.location.state._id : '')
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
                         <h1>Thank you, your application has been received</h1>
+                        {console.log(this.props)}
+                        {console.log(window.location.origin)}
                         <h3>Application ID: {this.props.location.state !== undefined && this.props.location.state._id}</h3>
-                        <button className="btn btn-success d-print-none" onClick={() => window.print()}>Print Confirmation</button>
+                        
                         <p>
-                            In porta placerat velit vitae fermentum. Phasellus eu ante gravida, ullamcorper sem id, efficitur magna.
-                            Nunc consectetur magna eu dictum aliquet. Aenean non cursus lacus, a accumsan diam.
-                            Nullam velit tortor, varius fringilla aliquet vitae, sagittis nec urna. Mauris mollis convallis libero, sed viverra augue. Donec sodales accumsan dolor a dignissim. Vivamus nec consectetur nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                    </p>
-                        <IconContext.Provider
-                            value={{
-                                color: "green",
-                                className: "bi",
-                                title: "success icon",
-                                size: "8em",
-                                style: { verticalAlign: 'middle' }
-                            }}>
-                            <div className="text-center">
-                                <BsCheckCircle />
+                            Your application has been successfully received. You can print this page for your records. A confirmation email has also been sent to the two contacts provided on the form.
+                        </p>
+                        <h4>Next Steps:</h4>
+                        <p>Please provide your participants the following instructions:</p>
+                        <div className="card">
+                            <div className="card-body">
+                                <p>Application ID: {this.props.location.state !== undefined && this.props.location.state._id}</p>
+                                <p>Please visit the following URL in order to provide your consent to the Ministry.</p>
+                                <p><a href={`${window.location.origin}/clientForm/${applicationId}`}>{`${window.location.origin}/clientForm/${applicationId}`}</a></p>
+                                <p>If you prefer a PDF version of the form, one can be found here. Once complete please email it to (email).</p>
                             </div>
-                        </IconContext.Provider>
-                        <br /><p>
-                            Sed pretium est vel eros lacinia lacinia. Sed ac arcu sed sapien eleifend mollis. Mauris ut risus at felis fermentum auctor quis vel arcu. Aenean pretium urna lacus, consequat convallis tellus porta vitae. Suspendisse tempus erat et leo ornare convallis. Etiam placerat ex sit amet interdum suscipit. Curabitur placerat nunc a posuere sollicitudin. Cras tincidunt dui eget nisl iaculis varius.
-                    </p>
+                        </div>
+                        <p>
+                            Note: you must get all your participants to complete the step above within the next 3 weeks.
+                        </p>
+                        <button className="btn btn-success d-print-none" onClick={() => window.print()}>Print Confirmation</button><br />
                     </div>
                 </div>
             </div>
