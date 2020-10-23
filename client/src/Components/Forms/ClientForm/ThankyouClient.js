99ab5e72f7cb3aae384de467a670b1a5c2fdc4b7
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2020-10-23 14:45:19 -0700

    removed extra space

--- client/src/Components/Forms/ClientForm/ThankyouClient.js
+++ client/src/Components/Forms/ClientForm/ThankyouClient.js
@@ -19,7 +19,7 @@ class ThankyouClient extends Component {
                         <h1>Thank you, your information has been received</h1>
                         <button className="btn btn-success d-print-none" onClick={() => window.print()}>Print Confirmation</button>
                         <br /><br />
-                        <p>Your participant information for the Work Experience Opportunities Grant has been successfully submitted. A confirmation email, along with a copy of the submission details have been sent to the email address provided.  </p>
+                        <p>Your participant information for the Work Experience Opportunities Grant has been successfully submitted. A confirmation email, along with a copy of the submission details have been sent to the email address provided.</p>
                         <p>Applications may take approximately four weeks for processing. The Ministry will contact your organization once the application has been processed, or if further information is required.</p>
                         <h3>For more information</h3>
                         <p>For more information on this grant opportunity, visit <a href="https://workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx" target="_blank" rel="noopener noreferrer">https://workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx</a></p>
