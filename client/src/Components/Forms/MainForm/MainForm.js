1730afb096b520c097313cfb1c0da91fb9da1749
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2020-12-07 15:08:08 -0800

    added messaging for period closure
    added messaging to main form and home for when application period is over

--- client/src/Components/Forms/MainForm/MainForm.js
+++ client/src/Components/Forms/MainForm/MainForm.js
@@ -98,10 +98,17 @@ class MainForm extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <ProgressTracker currentStep={this.state.currentStep}/>
-                        {this.state.hasError && (
+                        <div className="alert alert-primary" role="alert">
+                            <p><b>The Work Experience Opportunities Grant application window is closed. The good news is that we’re offering a second intake in January 2021. Stay tuned!</b></p>
+                            <p>Thanks for your interest in the Work Experience Opportunities Grant. Applications are no longer being accepted for the first intake period. A review is being conducted and several modifications to the program are being considered. Once finalized, a second intake will be available in January 2021. Please check back to stay informed of when the second intake period will be available.</p>
+                            <hr/>
+                            <p className="mb-0">If you have questions, please contact the ministry at workexperiencegrants@gov.bc.ca</p>
+                        </div>
+                        {/*<ProgressTracker currentStep={this.state.currentStep}/>*/}
+                        {/*this.state.hasError && (
                             generateAlert("alert-danger","An error has occurred, please refresh the page. If the error persists, please try again later.")
-                        )}
+                        )*/}
+                        {/*
                         <Formik
                             initialValues= {{
                                             _csrf: this.state._csrf,
@@ -217,6 +224,7 @@ class MainForm extends Component {
                                 </Form>
                             )}
                         </Formik>
+                            */}
                     </div>
                 </div>
             </div>
--- client/src/Home.js
+++ client/src/Home.js
@@ -6,14 +6,13 @@ function Home() {
             <div className="row">
                 <div className="col-md-12">
                     <h1>Work Experience Opportunities Grant Program</h1>
-                    <p>Welcome to the Work Experience Opportunities Grant Program application. Please click "start" below to get started. </p>
+                    <div className="alert alert-primary" role="alert">
+                            <p><b>The Work Experience Opportunities Grant application window is closed. The good news is that we’re offering a second intake in January 2021. Stay tuned!</b></p>
+                            <p>Thanks for your interest in the Work Experience Opportunities Grant. Applications are no longer being accepted for the first intake period. A review is being conducted and several modifications to the program are being considered. Once finalized, a second intake will be available in January 2021. Please check back to stay informed of when the second intake period will be available.</p>
+                            <hr/>
+                            <p className="mb-0">If you have questions, please contact the ministry at workexperiencegrants@gov.bc.ca</p>
+                    </div>
                     <p>For more information please click <a href="https://www.workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx" target="_blank" rel="noopener noreferrer">here.</a></p>       
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <h2>Get Started</h2>
-                    <p><a href="/form" className="btn btn-lg btn-primary">Start</a><br/></p>
                 </div>
             </div>
         </div>
