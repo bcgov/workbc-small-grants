8c35b1da6c02f174346f180fbdb0c8bd7909b332
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-01-25 14:15:21 -0800

    updated survey org landing, updated page 1 scale

--- client/src/Components/Forms/SurveyOrg/SurveyOrgStep1.js
+++ client/src/Components/Forms/SurveyOrg/SurveyOrgStep1.js
@@ -11,152 +11,160 @@ class SurveyOrgStep1 extends Component {
         //else return step 1
         return (
             <div>
-                <p>Thinking back about the overall application process, please rate the following:</p>
-                <div className="form-group">
-                    <label className="col-form-label control-label" htmlFor="easeOfNavigatingWebsite">1a. Ease of navigating the website and forms.<span style={{ color: "red" }}>*</span></label>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
-                            type="radio"
-                            name="easeOfNavigatingWebsite"
-                            value="1poor"
-                        />
-                        <label className="form-check-label">1 (poor)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
-                            type="radio"
-                            name="easeOfNavigatingWebsite"
-                            value="2fair"
-                        />
-                        <label className="form-check-label">2 (fair)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
-                            type="radio"
-                            name="easeOfNavigatingWebsite"
-                            value="3good"
-                        />
-                        <label className="form-check-label">3 (good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
-                            type="radio"
-                            name="easeOfNavigatingWebsite"
-                            value="4veryGood"
-                        />
-                        <label className="form-check-label">4 (very good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
-                            type="radio"
-                            name="easeOfNavigatingWebsite"
-                            value="5excellent"
-                        />
-                        <label className="form-check-label">5 (excellent)</label>
-                        {feedBackInvalid(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-form-label control-label" htmlFor="easeOfUnderstandingAppGuide">1b. Ease of understanding the application guide.<span style={{ color: "red" }}>*</span></label>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
-                            type="radio"
-                            name="easeOfUnderstandingAppGuide"
-                            value="1poor"
-                        />
-                        <label className="form-check-label">1 (poor)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
-                            type="radio"
-                            name="easeOfUnderstandingAppGuide"
-                            value="2fair"
-                        />
-                        <label className="form-check-label">2 (fair)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
-                            type="radio"
-                            name="easeOfUnderstandingAppGuide"
-                            value="3good"
-                        />
-                        <label className="form-check-label">3 (good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
-                            type="radio"
-                            name="easeOfUnderstandingAppGuide"
-                            value="4veryGood"
-                        />
-                        <label className="form-check-label">4 (very good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
-                            type="radio"
-                            name="easeOfUnderstandingAppGuide"
-                            value="5excellent"
-                        />
-                        <label className="form-check-label">5 (excellent)</label>
-                        {feedBackInvalid(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}
+                <p>Thinking back about the <b>online application process,</b> please rate your level of agreement with the following statements:</p>
+                <p>The rating scale used is as follows: 1 = strongly disagree, 2 = disagree, 3 = neither agree nor disagree, 4 = agree, 5 = strongly agree.</p>
+                <hr/>
+                <div className="form-group row">
+                    <label className="col-form-label control-label col-md-5" htmlFor="easeOfNavigatingWebsite">Ease of navigating the website and forms.<span style={{ color: "red" }}>*</span></label>
+                    <div className="col-md-7">
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
+                                type="radio"
+                                name="easeOfNavigatingWebsite"
+                                value="1stronglyDisagree"
+                            />
+                            <label className="form-check-label">1 (strongly disagree)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
+                                type="radio"
+                                name="easeOfNavigatingWebsite"
+                                value="2disagree"
+                            />
+                            <label className="form-check-label">2</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
+                                type="radio"
+                                name="easeOfNavigatingWebsite"
+                                value="3neitherAgreeNorDisagree"
+                            />
+                            <label className="form-check-label">3 (neither)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
+                                type="radio"
+                                name="easeOfNavigatingWebsite"
+                                value="4agree"
+                            />
+                            <label className="form-check-label">4</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}`}
+                                type="radio"
+                                name="easeOfNavigatingWebsite"
+                                value="5stronglyAgree"
+                            />
+                            <label className="form-check-label">5 (strongly agree)</label>
+                            {feedBackInvalid(this.props.errors, this.props.touched, "easeOfNavigatingWebsite")}
+                        </div>
                     </div>
                 </div>
-                <div className="form-group">
-                    <label className="col-form-label control-label" htmlFor="satisfactionSpeedOfService">1c. Satisfaction about the speed of service.<span style={{ color: "red" }}>*</span></label>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
-                            type="radio"
-                            name="satisfactionSpeedOfService"
-                            value="1poor"
-                        />
-                        <label className="form-check-label">1 (poor)</label>
+                <div className="form-group row">
+                    <label className="col-form-label control-label col-md-5" htmlFor="easeOfUnderstandingAppGuide">Ease of understanding the application guide.<span style={{ color: "red" }}>*</span></label>
+                    <div className="col-md-7">
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
+                                type="radio"
+                                name="easeOfUnderstandingAppGuide"
+                                value="1stronglyDisagree"
+                            />
+                            <label className="form-check-label">1 (strongly disagree)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
+                                type="radio"
+                                name="easeOfUnderstandingAppGuide"
+                                value="2disagree"
+                            />
+                            <label className="form-check-label">2</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
+                                type="radio"
+                                name="easeOfUnderstandingAppGuide"
+                                value="3neitherAgreeNorDisagree"
+                            />
+                            <label className="form-check-label">3 (neither)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
+                                type="radio"
+                                name="easeOfUnderstandingAppGuide"
+                                value="4agree"
+                            />
+                            <label className="form-check-label">4</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}`}
+                                type="radio"
+                                name="easeOfUnderstandingAppGuide"
+                                value="5stronglyAgree"
+                            />
+                            <label className="form-check-label">5 (strongly agree)</label>
+                            {feedBackInvalid(this.props.errors, this.props.touched, "easeOfUnderstandingAppGuide")}
+                        </div>
                     </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
-                            type="radio"
-                            name="satisfactionSpeedOfService"
-                            value="2fair"
-                        />
-                        <label className="form-check-label">2 (fair)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
-                            type="radio"
-                            name="satisfactionSpeedOfService"
-                            value="3good"
-                        />
-                        <label className="form-check-label">3 (good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
-                            type="radio"
-                            name="satisfactionSpeedOfService"
-                            value="4veryGood"
-                        />
-                        <label className="form-check-label">4 (very good)</label>
-                    </div>
-                    <div className="form-check">
-                        <Field
-                            className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
-                            type="radio"
-                            name="satisfactionSpeedOfService"
-                            value="5excellent"
-                        />
-                        <label className="form-check-label">5 (excellent)</label>
-                        {feedBackInvalid(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}
+                </div>
+                <div className="form-group row">
+                    <label className="col-form-label control-label col-md-5" htmlFor="satisfactionSpeedOfService">Satisfaction about the speed of service.<span style={{ color: "red" }}>*</span></label>
+                    <div className="col-md-7">
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
+                                type="radio"
+                                name="satisfactionSpeedOfService"
+                                value="1stronglyDisagree"
+                            />
+                            <label className="form-check-label">1 (strongly disagree)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
+                                type="radio"
+                                name="satisfactionSpeedOfService"
+                                value="2disagree"
+                            />
+                            <label className="form-check-label">2</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
+                                type="radio"
+                                name="satisfactionSpeedOfService"
+                                value="3neitherAgreeNorDisagree"
+                            />
+                            <label className="form-check-label">3 (neither)</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
+                                type="radio"
+                                name="satisfactionSpeedOfService"
+                                value="4agree"
+                            />
+                            <label className="form-check-label">4</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <Field
+                                className={`form-check-input ${feedBackClassName(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}`}
+                                type="radio"
+                                name="satisfactionSpeedOfService"
+                                value="5stronglyAgree"
+                            />
+                            <label className="form-check-label">5 (strongly agree)</label>
+                            {feedBackInvalid(this.props.errors, this.props.touched, "satisfactionSpeedOfService")}
+                        </div>
                     </div>
                 </div>
             </div>
