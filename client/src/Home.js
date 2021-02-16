bfda644de8a1aef6db3bf5cbbc571b16f0ea14f8
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-02-16 15:20:43 -0800

    updated period closed message
    updated period closed message for intake 2

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,9 +7,7 @@ function Home() {
                 <div className="col-md-12">
                     <h1>Work Experience Opportunities Grant Program</h1>
                     <div className="alert alert-primary" role="alert">
-                            <p><b>Applications are no longer being accepted for the first intake period of the Work Experience Opportunities Grant.</b></p>
-                            <p>Additional application intake opportunities are planned and the next application intake will be available in January 2021.</p>
-                            <p>Please check back here for updates and to stay informed of when the next intake period will be available.</p>
+                            <p><b>The Work Experience Opportunities Grant application window is now closed. The good news is that we’re offering another intake starting February 22, 2021. Check back then to find out what updates have been made to the program.</b></p>
                             <p></p>
                             <hr/>
                             <p className="mb-0">If you have questions, please email <a href="mailto:workexperiencegrants@gov.bc.ca">workexperiencegrants@gov.bc.ca</a></p>
