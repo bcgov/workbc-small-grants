7a1e938924698f7e0ac5d817f708cdcf8bbc786b
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-02-24 16:25:28 -0800

    updated matomo code

--- client/src/Components/Forms/SurveyOrg/ThankYouSurveyOrg.js
+++ client/src/Components/Forms/SurveyOrg/ThankYouSurveyOrg.js
@@ -11,8 +11,8 @@ class ThankyouSurveyOrg extends Component {
                         <p>Thank you for taking the time to help make the Work Experience Opportunities Grant program better. As an organization participating in the program, your views are very valuable and your contribution is much appreciated.</p>
                         <p>To find out more about WorkBC programs and services check out these links below highlighting some of our great programs for both employers and jobseekers:</p>
                         <ul>
-                            <li>The Wage Subsidy program covers a part of employee wages so employers can hire job seekers. Find out more by clicking <a href="https://www.workbc.ca/Employment-Services/Wage-Subsidy.aspx" target="_blank" rel="noopener noreferrer"> here.</a></li>
-                            <li>Learn more about all the resources available to employers through WorkBC by clicking <a href="https://www.workbc.ca/Employer-Resources.aspx" target="_blank" rel="noopener noreferrer"> here</a>.</li>
+                            <li>The Wage Subsidy program covers a part of employee wages so employers can hire job seekers. Find out more by clicking <a href="https://www.workbc.ca/Employment-Services/Wage-Subsidy.aspx" target="_blank" rel="noopener noreferrer" onclick="_paq.push(['trackEvent', 'ThankYouOrg', 'WageSubsidy']);"> here.</a></li>
+                            <li>Learn more about all the resources available to employers through WorkBC by clicking <a href="https://www.workbc.ca/Employer-Resources.aspx" target="_blank" rel="noopener noreferrer" onclick="_paq.push(['trackEvent', 'ThankYouOrg', 'LearnMore']);"> here</a>.</li>
                         </ul>
                     </div>
                 </div>
