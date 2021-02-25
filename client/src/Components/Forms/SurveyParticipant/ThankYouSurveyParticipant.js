136893f7663d4e42d2bb6410888ca36d9db9fe93
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-02-25 11:47:59 -0800

    removed extra char
    typo fix

--- client/src/Components/Forms/SurveyParticipant/ThankYouSurveyParticipant.js
+++ client/src/Components/Forms/SurveyParticipant/ThankYouSurveyParticipant.js
@@ -9,7 +9,7 @@ class ThankyouSurveyParticipant extends Component {
                     <div className="col-md-12">
                         <h1>Thank you, your information has been received.</h1>
                         <p>Thank you for providing your feedback and taking the time to help make the Work Experience Opportunities Grant program better. As a participant in the program, your views are very valuable and your contribution is much appreciated.</p>
-                        <p>To find out more about WorkBC programs and services, check out these links below highlighting some of our great programs for both employers and jobseekers:</p>'
+                        <p>To find out more about WorkBC programs and services, check out these links below highlighting some of our great programs for both employers and jobseekers:</p>
                         <ul>
                             <li>Discover job options and services designed to support British Columbians with disabilities by clicking <a href="https://www.workbc.ca/Resources-for/People-with-Disabilities.aspx" target="_blank" rel="noopener noreferrer" onclick="_paq.push(['trackEvent', 'ThankYouParticipant', 'Discover']);"> here.</a></li>
                             <li>The Wage Subsidy program covers a part of employee wages so employers can hire job seekers. Find out more by clicking <a href="https://www.workbc.ca/Employment-Services/Wage-Subsidy.aspx" target="_blank" rel="noopener noreferrer" onclick="_paq.push(['trackEvent', 'ThankYouParticipant', 'WageSubsidy']);">here</a>.</li>
