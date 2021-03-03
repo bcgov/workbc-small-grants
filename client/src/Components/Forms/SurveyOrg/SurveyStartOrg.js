fa5a63537fa5e6b7689103538dc86d2a989cce21
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-03-03 09:22:53 -0800

    updated survey landing page
    updated survey landing page to handle invalid links

--- client/src/Components/Forms/SurveyOrg/SurveyStartOrg.js
+++ client/src/Components/Forms/SurveyOrg/SurveyStartOrg.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import qs from 'qs'
+import { generateAlert } from '../Shared/Alert'
 
 class SurveyStartOrg extends Component {
+    constructor() {
+        super()
+        this.state = {
+            _uid: '',
+            invalidLink: false,
+        }
+    }
+
+    componentDidMount() {
+        let uid = qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).uid || ""
+        if (uid === "") {
+            this.setState({ invalidLink: true })
+        } else {
+            this.setState({ "_uid": uid })
+        }
+    }
     render() {
         return (
             <div className="container">
-                {console.log(this.props)}
                 <div className="row">
                     <div className="col-md-12">
                         <h1>Work Experience Opportunities Grant Organization Survey</h1>
+                        {(this.state.invalidLink || typeof this.props.match.params.intake === 'undefined') && (
+                            generateAlert("alert-danger", "Invalid link, please use the link that was sent to you through email.")
+                        )}
                         <p>
                             Welcome to the Work Experience Opportunities Grant survey.
                         </p>
@@ -37,7 +57,12 @@ class SurveyStartOrg extends Component {
                         </ul>
                         <p>Please note that all information is being collected under the authority of the <a href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/96165_03#section26" target="_blank" rel="noopener noreferrer">Freedom of Information and Protection of Privacy Act, Section 26(e).</a> If you have any questions about the collection or use of this information, please contact the <a href="mailto:workexperiencegrants@gov.bc.ca">Ministry of Social Development and Poverty Reduction.</a></p>
                         <p>Thank you for participating – your contribution will make a difference in shaping how the Work Experience Opportunities Grant program is offered in the future!</p>
-                        <p><a href={`/surveyOrg/${(typeof this.props.match.params.intake !== 'undefined') ? this.props.match.params.intake : ''}`} className="btn btn-lg btn-primary">Start</a><br /></p>
+                        <p>
+                            <a 
+                                href={`/surveyOrg/${(typeof this.props.match.params.intake !== 'undefined') ? this.props.match.params.intake : ''}${this.state._uid !== "" ? `?uid=${this.state._uid}` : ''}`} 
+                                className={`btn btn-lg btn-primary ${(this.state.invalidLink || typeof this.props.match.params.intake === 'undefined') && "disabled"}`}
+                            >Start
+                            </a><br /></p>
                     </div>
                 </div>
 
