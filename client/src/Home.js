b6efc8a6d4dc73e8d916dcd06cfd92ea4bd7c5b7
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2022-01-06 16:49:39 -0800

    updated home page for new intake

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,7 +4,9 @@ function Home() {
     return (
         <div className="container">
             <div className="row">
+
                 <div className="col-md-12">
+                    {/*
                     <div className="alert alert-primary" role="alert">
                         <p><b>The Work Experience Opportunities Grant application window is now closed. Thanks for your interest in this funding opportunity. Check back to learn about how many participants and organizations were supported through this program as well as potential future funding opportunities.</b></p>
                         <p></p>
@@ -13,19 +15,17 @@ function Home() {
                     </div>
                     <p>For more information please click <a href="https://www.workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx" target="_blank" rel="noopener noreferrer">here.</a></p>   
                 </div>
-                {/* 
+                */}
                     <h1>Work Experience Opportunities Grant Program</h1>
                     <p>Welcome to the Work Experience Opportunities Grant Program application. Please click "start" below to get started. </p>
-                    <p>For more information please click <a href="https://www.workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx" target="_blank" rel="noopener noreferrer">here.</a></p>       
+                    <p>For more information please click <a href="https://www.workbc.ca/Employment-Services/Work-Experience-Opportunities-Grant.aspx" target="_blank" rel="noopener noreferrer">here.</a></p>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-12">
                     <h2>Get Started</h2>
-                    <p><a href="/form" className="btn btn-lg btn-primary">Start</a><br/></p>
+                    <p><a href="/form" className="btn btn-lg btn-primary">Start</a><br /></p>
                 </div>
-            </div>
-            */}
             </div>
         </div>
     )
