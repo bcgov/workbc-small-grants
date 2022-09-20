599483f022d92fef7c1bff2696c038d568ae794a
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2022-09-20 16:24:04 -0700

    update message for form unavailable

--- client/src/Components/Forms/ClientForm/FormUnavailable.js
+++ client/src/Components/Forms/ClientForm/FormUnavailable.js
@@ -7,7 +7,7 @@ function FormUnavailable() {
                 <div className="col-md-12">
                     <div className="alert alert-primary" role="alert">
                         <p>Thank you for your interest in the Work Experience Opportunities Grant Program. This Participant Information link is no longer valid.</p>
-                        <p>If you are a participant of the Work Experience Opportunities Grant Program that launched on February 2nd, 2022, please contact the organization who is supporting your Work Experience Placement for an updated link.</p>
+                        <p>Please contact the organization who is supporting your Work Experience Placement.</p>
                     </div>
                 </div>
             </div>
